3e6a8d600af318368b27395ac963cfb5961da004
ThePratikSah <shivam3448@gmail.com>
2021-01-31 21:55:50 +0530

    Made app static

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,53 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Image from 'next/image/';
+import Header from "../components/header";
+import Menu from "../components/menu";
+import Contents from "../components/content";
 
-export default function Home() {
+export default function HomePage({ data }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>The Midtown Cafe</title>
-      </Head>
-      <main className={styles.main}>
-        <Image src={"/images/food.svg"} width={250}
-               height={250} alt="Food"/>
-        <h1 className={styles.title}>
-          The Midtown Cafe
-        </h1>
-        <p>Website under construction</p>
-        <a className={styles.visit}
-           href="https://www.google.com/maps/place/The+Midtown+Cafe/@25.7686372,87.4703869,17z/data=!3m1!4b1!4m5!3m4!1s0x39eff916e521eddb:0xb9190efcc817d4fc!8m2!3d25.7686372!4d87.4725756">Visit
-          Us</a>
-      </main>
+    <div>
+      <Header />
+      <Menu />
+      {data.menu.map((singleData, index) => <Contents key={index} id={index} singleData={singleData} />)}
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const data = {
+    "menu": [
+      {
+        "momos": [
+          {
+            "name": "Veg Momos",
+            "price": 40,
+            "image": "momos.jpg"
+          },
+          {
+            "name": "Non-Veg Momos",
+            "price": 50,
+            "image": "momos.jpg"
+          }
+        ]
+      },
+      {
+        "rolls": [
+          {
+            "name": "Veg Roll",
+            "price": 30,
+            "image": "rolls.jpg"
+          },
+          {
+            "name": "Egg Roll",
+            "price": 40,
+            "image": "rolls.jpg"
+          }
+        ]
+      }
+    ]
+  }
+  return {
+    props: {
+      data
+    }
+  }
 }
