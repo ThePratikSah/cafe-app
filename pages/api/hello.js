aaf8da8e1bf78250c83764d14e4e7a227c93c8ca
ThePratikSah <shivam3448@gmail.com>
2021-01-31 18:24:02 +0530

    .

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({name: 'John Doe'})
+export default function handler(req, res) {
+  res.status(200).json(
+    {
+      "menu": [
+        {
+          "momos": [
+            {
+              "name": "Veg Momos",
+              "price": 40,
+              "image": "momos.jpg"
+            },
+            {
+              "name": "Non-Veg Momos",
+              "price": 50,
+              "image": "momos.jpg"
+            }
+          ]
+        },
+        {
+          "rolls": [
+            {
+              "name": "Veg Roll",
+              "price": 30,
+              "image": "rolls.jpg"
+            },
+            {
+              "name": "Egg Roll",
+              "price": 40,
+              "image": "rolls.jpg"
+            }
+          ]
+        }
+      ]
+    }
+  );
 }
--- pages/home.js
+++ pages/home.js
@@ -2,18 +2,18 @@ import Header from "../components/header";
 import Menu from "../components/menu";
 import Contents from "../components/content";
 
-export default function HomePage({data}) {
+export default function HomePage({ data }) {
   return (
     <div>
-      <Header/>
-      <Menu/>
-      {data.menu.map((singleData, index) => <Contents key={index} id={index} singleData={singleData}/>)}
+      <Header />
+      <Menu />
+      {data.menu.map((singleData, index) => <Contents key={index} id={index} singleData={singleData} />)}
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const res = await fetch('http://localhost:3000/data.json');
+  const res = await fetch('https://themidtowncafe.com/api/hello');
   const data = await res.json();
   return {
     props: {
